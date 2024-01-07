b196b41de3c092be05f1b05526c94d99f1987d83
Tyler-McIntyre <tmcintyre802@gmail.com>
2024-01-07 07:34:19 -0500

    push challenges

---
+++ unity/Create With Code/Challenge 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 1;
+    public float rotationSpeed = 10;
+    public float verticalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        var propellerObject = transform.Find("Propeller").gameObject;
+
+        if (propellerObject != null)
+        {
+            propellerObject.transform.Rotate(Vector3.forward, rotationSpeed);
+            //MeshFilter propellerMeshFilter = propellerObject.GetComponent<MeshFilter>();
+        }
+        else
+        {
+            Debug.Log("Propeller object not found");
+        }
+
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * speed);
+
+        float angle = verticalInput * rotationSpeed * Time.deltaTime;
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right, angle);
+    }
+}
---
+++ unity/Create With Code/Challenge 2/Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class SpawnManagerX : MonoBehaviour
+{
+    public GameObject[] ballPrefabs;
+    public GameObject[] dogPrefabs;
+
+    private readonly float spawnLimitXLeft = -33;
+    private readonly float spawnLimitXRight = 11;
+    private readonly float spawnPosY = 30;
+    public float timeBetweenKeyDown = 0.05f;
+    private float timeStamp;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(nameof(SpawnRandomBall));
+    }
+
+    private void Update()
+    {
+        // prevent rapid fire
+        bool canSpawnDog = Time.time >= timeStamp;
+
+        // instantiate dog on space bar pressed
+        if (canSpawnDog && Input.GetKeyDown(KeyCode.Space))
+        {
+            Vector3 spawnPos = new(gameObject.transform.position.x, 0, 0); // spawn from player position
+            GameObject dogPrefab = dogPrefabs[0];
+            Instantiate(dogPrefab, spawnPos, dogPrefab.transform.rotation);
+
+            // set new timestamp to check if enough time has elappsed
+            timeStamp = Time.time + timeBetweenKeyDown;
+        }
+    }
+
+    // Spawn random ball at random x position at top of play area
+    IEnumerator SpawnRandomBall()
+    {
+        while(true)
+        {
+            // Generate random ball index and random spawn position
+            Vector3 spawnPos = new(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
+
+            var randomBallPrefabIndex = Random.Range(0, ballPrefabs.Length);
+
+            // instantiate ball at random spawn location
+            Instantiate(ballPrefabs[randomBallPrefabIndex], spawnPos, ballPrefabs[randomBallPrefabIndex].transform.rotation);
+
+            // pass control back to unity after each object is returned
+            yield return new WaitForSeconds(Random.Range(3.0f, 5.0f));
+        }
+    }
+}
